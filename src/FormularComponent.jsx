56cd2d90bb5d1d57e3a76722c5bbf4b344541d25
Teri Chudějová <tereza.chudejova@gmail.com>
2024-09-11 23:32:09 +0200

    change path to the landing page image

--- src/FormularComponent.jsx
+++ src/FormularComponent.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import "./FormularComponent.css";
 
+
 const FormularComponent = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
